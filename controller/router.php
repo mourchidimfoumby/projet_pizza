d02b4e355a68be3c8e2c2acb92c6519ba1ac8f2c
Mourchidi MFOUMBY <mfoumbymourchidi@gmail.com>
2023-12-29 17:48:02 +0100

    feat : ajout page d'accueil par défault

--- controller/router.php
+++ controller/router.php
@@ -1,7 +1,8 @@
 <?php
 
-//valeur par défaut de l'objet
-$objet = "pizza";
+require_once("config/connexion.php");
+connexion::connect();
+
 //les objets possibles
 $objets = [
     "pizza",
@@ -13,32 +14,11 @@ $objets = [
 if (isset($_GET["objet"]) && in_array($_GET["objet"], $objets)) {
     //si c'est le cas, on récupère l'objet passé dans l'url
     $objet = $_GET["objet"];
+    $controller = "controller". ucfirst($objet);
+    require_once("controller/$controller.php");
+    $controller::displayAll();
 }
-//construction du contrôlleur
-$controller = "controller". ucfirst($objet);
-//insertion du contrôleur
-require_once("controller/$controller.php");
-//connexion
-require_once("config/connexion.php");
-connexion::connect();
-//appel de la méthode displayAll du contrôleur
-if(isset($_GET["action"])){
-    $action = $_GET["action"];
-    switch ($action) 
-    {
-        case "displayOne":
-            $controller::displayOne();
-            break;
-        case "delete":
-            $controller::delete();
-            break;
-        case "displayCreationForm":
-            $controller::displayCreationForm();
-            break;
-        case "create":
-            $controller::create();
-            break;
-    }
+else{
+    include("view/home.php");
 }
-else $controller::displayAll();
 ?>
---
+++ view/home.php
@@ -0,0 +1,7 @@
+<?php
+
+include("view/head.php");
+include("view/navbar.html");
+echo '<h1 style="margin: auto; height: 50vh;">Hello bienvenue sur notre site !</h1>';
+include("view/footer.html");
+?>
