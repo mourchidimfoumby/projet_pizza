adf0d8ddc1f2c278c0a274b8693986acbd335732
Mourchidi MFOUMBY <mfoumbymourchidi@gmail.com>
2023-12-30 16:01:57 +0100

    feat : ajout des polices

--- view/home.php
+++ view/home.php
@@ -2,6 +2,6 @@
 
 include("view/head.php");
 include("view/navbar.html");
-echo '<h1 style="margin: auto; height: 50vh;">Hello bienvenue sur notre site !</h1>';
+echo '<h2 style="margin: auto; height: 50vh;">Hello bienvenue sur notre site !</h2>';
 include("view/footer.html");
 ?>
