286c7c3e056ae94061c404845fcf46f33bdf6e7e
Mourchidi MFOUMBY <mfoumbymourchidi@gmail.com>
2024-01-05 17:26:01 +0100

    fix: bug affichage prix popup + séparationd es fichier js

---
+++ js/popup.js
@@ -0,0 +1,44 @@
+
+export const popup = {
+    close : function (objet) {
+    window.location.href = "index.php?objet=" + objet;
+  },
+
+  removeIngredient : function (btnSpanRemove) {
+
+    let spanIngredient = $(btnSpanRemove).closest("span.popup-list-add-price")
+    let spanBtnAdd = $("#popup-btn-add").find("span#total-price").first();
+    let itemPrice = 0;
+    if(spanIngredient.length > 0) itemPrice = parseFloat(spanIngredient.text());
+    let totalPrice = parseFloat(spanBtnAdd.text());
+    totalPrice -= itemPrice;
+    totalPrice = totalPrice.toFixed(2);
+    spanBtnAdd.text(totalPrice);
+
+    let itemList = $(btnSpanRemove).closest(".popup-list-remove");
+    itemList.removeClass("popup-list-remove").addClass("popup-list-add");
+    itemList.appendTo("#popup-ul-add");
+
+    $(btnSpanRemove).removeClass("bi-x-circle");
+    $(btnSpanRemove).addClass("bi-plus-lg");
+
+  },
+
+  addIngredient: function (itemList) {
+    let span = $(itemList).find("span.bi-plus-lg").first();
+    span.removeClass("bi-plus-lg");
+    span.addClass("bi-x-circle");
+
+    let spanIngredient = $(itemList).find("span.popup-list-add-price").first();
+    let spanBtnAdd = $("#popup-btn-add").find("span#total-price").first();
+
+    let itemPrice = parseFloat(spanIngredient.text());
+    let totalPrice = parseFloat(spanBtnAdd.text());
+    totalPrice += itemPrice;
+    totalPrice = totalPrice.toFixed(2);
+    spanBtnAdd.text(totalPrice);
+
+    $(itemList).removeClass("popup-list-add").addClass("popup-list-remove");
+    $(itemList).appendTo("#popup-ul-remove");
+  }
+};
--- js/script.js
+++ js/script.js
@@ -1,169 +1,42 @@
+import {cart} from "./cart.js";
+import {popup} from "./popup.js";
+
 $(function () {
   /*============= VARIABLE GLOBALES ==============*/
 
-  let params = new URLSearchParams(window.location.search);
+  const params = new URLSearchParams(window.location.search);
   let objet = params.get("objet");
   let id_pizza = params.get("id_pizza");
   let parameters = {
     objet: objet,
   };
 
-  /*============= FONCTIONS ==============*/
-
-  function addProductToCart(productItem) {
-    parameters.id = $(productItem).data("id_product");
-
-    $.ajax({
-      type: "POST",
-      url: "controller/treatment_cart.php",
-      data: {
-        action: "addProductToCart",
-        parameters: parameters,
-      },
-      dataType: "json",
-    })
-      .done(function (response) {
-        console.log("Réponse du serveur :", response);
-        location.reload();
-
-      })
-      .fail(function (xhr, status) {
-        console.error(
-          "Erreur AJAX (statut " + status + ") :",
-          xhr.responseText
-        );
-      });
-  }
-
-  function removeToCart(cartItem) {
-    parameters.position = $(cartItem).data("position");
-    $.ajax({
-      type: "POST",
-      url: "controller/treatment_cart.php",
-      data: {
-        action: "removeToCart",
-        parameters: parameters,
-      },
-      dataType: "json",
-    })
-      .done(function (response) {
-        console.log("Réponse du serveur :", response);
-        location.reload();
-      })
-      .fail(function (xhr, status) {
-        console.error(
-          "Erreur AJAX (statut " + status + ") :",
-          xhr.responseText
-        );
-      });
-  }
-
-  function closePopup() {
-    window.location.href = "index.php?objet=" + objet;
-  }
-
-  function removeIngredientPopup(btnSpanRemove) {
-    $(btnSpanRemove).removeClass("bi-x-circle");
-    $(btnSpanRemove).addClass("bi-plus-lg");
-
-    let itemList = $(btnSpanRemove).closest(".popup-list-remove");
-    itemList.removeClass("popup-list-remove").addClass("popup-list-add");
-    itemList.appendTo("#popup-ul-add");
-
-    let spanIngredient = $(btnSpanRemove).find("span.popup-list-add-price").first();
-    let spanBtnAdd = $("#popup-btn-add").find("span#total-price").first();
-
-    let itemPrice = parseFloat(spanIngredient.text());
-    let totalPrice = parseFloat(spanBtnAdd.text());
-    totalPrice -= itemPrice;
-    totalPrice = totalPrice.toFixed(2);
-    spanBtnAdd.text(totalPrice);
-  }
-
-  function addIngredientPopup(itemList) {
-    let span = $(itemList).find("span.bi-plus-lg").first();
-    span.removeClass("bi-plus-lg");
-    span.addClass("bi-x-circle");
-
-    let spanIngredient = $(itemList).find("span.popup-list-add-price").first();
-    let spanBtnAdd = $("#popup-btn-add").find("span#total-price").first();
-
-    let itemPrice = parseFloat(spanIngredient.text());
-    let totalPrice = parseFloat(spanBtnAdd.text());
-    totalPrice += itemPrice;
-    totalPrice = totalPrice.toFixed(2);
-    spanBtnAdd.text(totalPrice);
-
-    $(itemList).removeClass("popup-list-add").addClass("popup-list-remove");
-    $(itemList).appendTo("#popup-ul-remove");
-  }
-
-    function addPizzaToCart(){
-      let pizzaIngredientsList = $("#popup-ul-remove").find("li");
-      if(pizzaIngredientsList.length === 0) alert("Veuillez insérer au moins un ingrédient dans la pizza");
-      else
-      {
-
-        let pizzaIngredients = {};
-        pizzaIngredientsList.each(function(){
-          let span =  $(this).find("span.popup-list-add-price").first();
-          span.removeClass("popup-list-add-price");
-          span.text("");
-          let nameIngredient = $(this).text();
-          let dataId = $(this).data("id_ingredient");
-          pizzaIngredients[dataId] = nameIngredient;
-        });
-
-          parameters.id = id_pizza;
-          parameters.ingredients = pizzaIngredients;
-          $.ajax({
-            type: "POST",
-            url: "controller/treatment_cart.php",
-            data: {
-              action: "addPizzaToCart",
-              parameters: parameters,
-            },
-            dataType: "json",
-          })
-            .done(function (response) {
-              console.log("Réponse du serveur :", response);
-              closePopup();
-            })
-            .fail(function (xhr, status) {
-              console.error(
-                "Erreur AJAX (statut " + status + ") :",
-                xhr.responseText
-              );
-            });
-        }
-    }
 
   /*============= EVENEMENTS ==============*/
 
-  $(".bi-x").on("click", closePopup);
+  $(".bi-x").on("click", function(){
+    popup.close(objet)});
 
-  $("#popup-btn-add").on("click", function () {
-    addPizzaToCart();
-  });
+  $("#popup-btn-add").on("click", function(){
+    cart.addPizza(parameters, id_pizza, objet)});
 
-  $("#cart-item .bi-x-circle").on("click", function () {
-    removeToCart(this);
-  });
+  $("#cart-item .bi-x-circle").on("click", function(){
+    cart.removeItem(this, parameters)});
 
   $(".other-product").on("click", function (event) {
     event.preventDefault();
-    addProductToCart(this);
+    cart.addProduct(this, parameters);
   });
 
   $("#popup-ul-remove").on(
     "click",
     ".popup-list-remove .bi-x-circle",
     function () {
-      removeIngredientPopup(this);
+      popup.removeIngredient(this);
     }
   );
 
   $("#popup-ul-add").on("click", ".popup-list-add", function () {
-    addIngredientPopup(this);
+    popup.addIngredient(this);
   });
 });
